a1e54e1e9771b3125aa8341362733a365d5e6e12
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-03-29 16:20:35 +0200

    Ajout du fichier script.js et fonctions incrémentation et décrémentation

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var data = 0;
+  
+//Valeur par défaut : 0 dans le h2 de compteur
+document.getElementById("counting").innerText = data;
+  
+//Fonction incrément (+)
+function increment() {
+    data = data + 1;
+    document.getElementById("counting").innerText = data;
+}
+
+//Fonction décrément (-)
+function decrement() {
+    data = data - 1;
+    document.getElementById("counting").innerText = data;
+}
